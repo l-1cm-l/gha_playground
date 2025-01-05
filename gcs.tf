549c7505c96b119d72af7f95537593655e71c53c
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-01-05 14:32:09 +0000

    feat: update GitHub Actions workflow to ignore master branch and add new GCS bucket resource

--- gcs.tf
+++ gcs.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "test_bucket_202501052228" {
   name     = "test-bucket-202501052228"
   location = "asia-northeast1"
 }
+
+resource "google_storage_bucket" "test_bucket_202501052228_ex" {
+  name     = "test-bucket-202501052228-ex"
+  location = "asia-northeast1"
+}
