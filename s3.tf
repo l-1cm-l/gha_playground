dccca85774b90852219427fe4d4fb6207e2fdb3f
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-02-12 07:05:54 +0000

    fix: Remove unused S3 bucket resource and update Terraform version constraints

--- s3.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "202502121551"
-  acl    = "private"
-}
--- version.tf
+++ version.tf
@@ -1,9 +1,9 @@
 terraform {
+  required_version = ">= 1.10.3" # Terraform 自体のバージョン制約（例）
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.33.0"
+      version = "~> 4.0" # 適宜バージョンを指定してください
     }
   }
-  required_version = "1.10.3"
 }
