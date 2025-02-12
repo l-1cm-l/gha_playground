81a6a92b6d947c85716fa99be9e06e89674a0ac9
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-02-12 07:38:41 +0000

    fix: Remove unused GCS bucket and add new S3 bucket configuration

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "example" {
   bucket = "202502121629t"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "example_ex" {
   bucket = "202502121629t-ex"
 }
 
