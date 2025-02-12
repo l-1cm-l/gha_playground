dbe3003e56bcb26da7ccc550e6fbf4e689e43f2d
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-02-12 07:07:18 +0000

    fix: Remove unused S3 bucket resource and update Terraform version constraints

--- gcs.tf
+++ gcs.tf
@@ -1,14 +1,1 @@
-resource "google_storage_bucket" "test_bucket_202501052228" {
-  name     = "test-bucket-202501052228"
-  location = "asia-northeast1"
-}
 
-resource "google_storage_bucket" "test_bucket_202501052228_ex" {
-  name     = "test-bucket-202501052228-ex"
-  location = "asia-northeast1"
-}
-
-resource "google_storage_bucket" "test_bucket_202501052228_ex_1" {
-  name     = "test-bucket-202501052228-ex-1"
-  location = "asia-northeast1"
-}
