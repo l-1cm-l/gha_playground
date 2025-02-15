752aa7fb09976aa1c9cf8b5dcb1d40523ce69911
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-02-15 12:53:46 +0000

    fix: Use local variable for GCS bucket location in Japan East

--- gcs.tf
+++ gcs.tf
@@ -1,14 +1,18 @@
+locals {
+  japan_east = "asia-northeast1"
+}
+
 resource "google_storage_bucket" "test_bucket_202501052228" {
   name     = "test-bucket-202501052228"
-  location = "asia-northeast1"
+  location = local.japan_east
 }
 
 resource "google_storage_bucket" "test_bucket_202501052228_ex" {
   name     = "test-bucket-202501052228-ex"
-  location = "asia-northeast1"
+  location = local.japan_east
 }
 
 resource "google_storage_bucket" "test_bucket_202501052228_ex2" {
   name     = "test-bucket-202501052228-ex2"
-  location = "asia-northeast1"
+  location = local.japan_east
 }
