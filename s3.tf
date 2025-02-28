0caaa64cbc9a091ee65e4ec54407d5d3242a4893
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-02-28 13:19:51 +0000

    use  lazygit
    feat: Add additional GCS and S3 resources
    
    fix: Use local variable for GCS bucket location in Japan East

--- s3.tf
+++ s3.tf
@@ -6,3 +6,10 @@ resource "aws_s3_bucket" "example_ex" {
   bucket = "202502121629t-ex"
 }
 
+resource "aws_s3_bucket" "example_ex_2" {
+  bucket = "202502121629t-ex-2"
+}
+
+resource "aws_s3_bucket" "example_ex_3" {
+  bucket = "202502121629t-ex-3"
+}
