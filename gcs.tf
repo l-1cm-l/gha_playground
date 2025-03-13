a197074b044495fb9b1ac212665e771501b64809
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-03-13 05:55:51 +0000

    feat: add additional GCS bucket for testing
    feat: add additional GCS bucket for testing

--- gcs.tf
+++ gcs.tf
@@ -11,3 +11,8 @@ resource "google_storage_bucket" "test_bucket_202503131452" {
   name     = "test-bucket-202503131452"
   location = local.japan_east
 }
+
+resource "google_storage_bucket" "test_bucket_202503131455" {
+  name     = "test-bucket-202503131455"
+  location = local.japan_east
+}
