4a3114cd087350a7e84dafe8333f189eb16eb544
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-03-27 12:30:02 +0000

    fix: correct resource group name in import configuration

--- import.tf
+++ import.tf
@@ -1,5 +1,5 @@
 import {
   id = "NetworkWatcherRG"
-  to = "/subscriptions/${var.subscription_id}/resourceGroups/NetworkWatcherRG"
+  to = "/subscriptions/57bbe82b-1d10-4e37-bba6-566ae5048c85/resourceGroups/NetworkWatcherRG"
 }
 
