5c9fa47fac9136089c6a58d4bac592bc8bc1367c
l-1cm-l <l-----1.5cm----l@kyf.biglobe.ne.jp>
2025-03-31 05:55:53 +0000

    chore: add CODEOWNERS file and update Azure provider configuration

--- main.tf
+++ main.tf
@@ -8,10 +8,15 @@ provider "aws" {
 }
 
 provider "azurerm" {
+<<<<<<< Updated upstream
   features {
     resource_group {
       prevent_deletion_if_contains_resources = true
     }
   }
   subscription_id = var.subscription_id
+=======
+  features {}
+  subscription_id = "dev-ss-444303"
+>>>>>>> Stashed changes
 }
